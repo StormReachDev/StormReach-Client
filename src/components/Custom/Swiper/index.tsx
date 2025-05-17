ef7a5f2333fae705b2093fee2af6ad2633d7dee3
Abdul Rehan <rehannajam2@gmail.com>
2025-05-17 17:01:48 +0500

    feat(swiper): add customizable Marquee component

---
+++ src/components/Custom/Swiper/index.tsx
@@ -0,0 +1,22 @@
+// Imports:
+import { swiper } from '@/constants/Paths';
+import Image from 'next/image';
+import Marquee from 'react-fast-marquee';
+
+export default function Swiper() {
+  return (
+    <Marquee speed={100}>
+      {swiper.map((swiper) => (
+        <div key={swiper.alt} className="flex items-center size-full px-5">
+          <Image
+            src={swiper.image}
+            alt={swiper.alt}
+            width={500}
+            height={500}
+            className="object-cover size-full"
+          />
+        </div>
+      ))}
+    </Marquee>
+  );
+}
---
+++ src/constants/Paths/index.ts
@@ -0,0 +1,39 @@
+export const swiper = [
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+  {
+    image: '/images/logo.png',
+    alt: 'Image 1',
+  },
+];
+
+export const pricing = {
+  approvalIcon: '/images/list-approval.png',
+  approvalIconAlt: 'Approval Icon',
+};
